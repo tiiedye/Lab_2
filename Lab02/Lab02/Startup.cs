d4687dea3a410f756b4ae16ac14097febf127042
tiiedye <tiiedye@gmail.com>
2020-10-26 12:34:23 -0700

    adding project files

---
+++ Lab02/Lab02/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab02.Startup))]
+namespace Lab02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
